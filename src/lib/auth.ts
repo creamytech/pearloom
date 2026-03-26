2314764f1227e1579565b0399f5a53a7357d387f
creamytech <benjaminscott18@gmail.com>
2026-03-26 14:43:05 -0400

    upgrade to primary photoslibrary scope to resolve google 403 strict matching errors

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,7 +37,7 @@ export const authOptions: NextAuthOptions = {
             'openid',
             'email',
             'profile',
-            'https://www.googleapis.com/auth/photoslibrary.readonly',
+            'https://www.googleapis.com/auth/photoslibrary',
           ].join(' '),
           access_type: 'offline',
           prompt: 'consent',
